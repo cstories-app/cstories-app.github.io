c9b9b74437705363d804fbfc4f76f5d767b09763
Jonathan Zadra <jzadra@integral-corp.com>
2022-12-19 14:44:51 -0700

    rename news code dir

--- news_code/news_apis.R
+++ news_code/news_apis.R
@@ -12,6 +12,7 @@ library(httr2)
 library(rrapply)
 library(janitor)
 library(fs)
+library(lubridate)
 
 # Functions ---------------------------------------------------------------
 
@@ -30,12 +31,17 @@ news <- terms %>%
   map(function(search_terms) {
 
     #TODO Take a look at https://httr2.r-lib.org/reference/multi_req_perform.html
+  print(search_terms)
 
-    req %>%
+    resp <- req %>%
       req_url_query(q = search_terms) %>%
       req_url_query(apiKey = news_api_key) %>%
       req_perform() %>%
-      resp_body_json() %>%
+      resp_body_json()
+
+    if(resp$totalResults == 0) return(NULL)
+
+    resp %>%
       rrapply(how = "melt") %>%
       as_tibble() %>%
       slice(-(1:2)) %>%
@@ -46,10 +52,18 @@ news <- terms %>%
       deselect(L1) %>%
       clean_names() %>%
       rename(source = name) %>%
-      add_column(terms = search_terms) %>%
-      mutate()
+      add_column(terms = search_terms)
   }) %>%
   bind_rows()
+
+news <- news %>%
+  mutate(published_at = as_date(published_at) %>% as.character()) %>%
+  mutate(author = replace_na(author, "(unknown author)"),
+         description = replace_na(description, "(no description)")) %>%
+  deselect(id) %>%
+  rename(image = url_to_image)
+
+
 #
 # if(fs::file_exists("news/data/news_table.rds")) {
 #   news_table <- read_rds("news/data/news_table.rds")
@@ -65,3 +79,23 @@ news %>%
 
 
 
+
+
+
+news %>%
+  deselect(l2, terms) %>%
+  mutate(rn = row_number()) %>%
+  pivot_longer(-rn) %>%
+  mutate(value = paste0("\"", value, "\"")) %>%
+  unite(col = yaml, name, value, sep = ": ") %>%
+  #mutate(yaml = str_wrap(yaml, exdent = 3)) %>%
+  group_by(rn) %>%
+  summarize(yaml = paste(yaml, collapse = "\n")) %>%
+  mutate(yaml = paste0("---\n", yaml, "\n---\n")) %>%
+  rowwise() %>%
+  pwalk(function(...) {
+    x <- tibble(...)
+    yaml <- x %>% pull(yaml)
+    fileid <- x %>% pull(rn)
+    write_lines(yaml, file = paste0("news-qmds/", fileid, ".qmd"))
+  })
