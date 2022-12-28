dc9ae4cccb3d4c423fc9699f9d553564f23fd351
Jonathan Zadra <jzadra@integral-corp.com>
2022-12-28 12:29:50 -0700

    Add logic to skip feeds that are "commented out" in the text file.

--- news_code/news_rss.R
+++ news_code/news_rss.R
@@ -3,12 +3,29 @@ library(integral)
 library(tidyRSS)
 
 
-sb_rss <- read_lines("news/rss_feeds_santa_barbara.txt")
+sb_rss <- read_lines("news_code/rss_feeds_santa_barbara.txt")
 
-sb_rss %>%
-  map(~tidyfeed(.x)) %>%
+feed_sb <- sb_rss %>%
+  map(function(feed) {
+
+    if(str_detect(feed, "^!")) {
+      cli::cli_alert_danger("Skipping {feed}
+                            \r")
+      return(NULL)
+    }
+
+    cli::cli_alert_info("Reading {feed}...")
+
+    return(tidyfeed(feed))
+
+    }) %>%
   bind_rows()
 
 keyt <- tidyfeed("keyt.com/feed")
 
 keyt %>% filter(str_detect(item_description, "wind"))
+
+keyt %>% slice(1) %>% glimpse
+
+
+
